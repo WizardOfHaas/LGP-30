5b9ba4d62c9dc4938c48492fa7b988b81aacaf3e
Marc Anderson <marccand@outlook.com>
2025-11-15 18:27:38 +1100

    added eslint

---
+++ ts-lgp-30/eslint.config.js
@@ -0,0 +1,12 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm" },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css" },
+]);
